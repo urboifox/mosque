f2890cc3fb6d406a43552bc163dfe7ec6bef9ac7
Mohamed Ashraf <mohamed.dev.egy@gmail.com>
2024-03-15 13:54:42 +0200

    removed active status

--- src/app/[locale]/private-files/components/PrivateFileCard.tsx
+++ src/app/[locale]/private-files/components/PrivateFileCard.tsx
@@ -14,25 +14,23 @@ export default function PrivateFileCard({
   const { title, description } = selectTranslation(locale, data);
   const t = useTranslations();
   return (
-    data.activeState && (
-      <article className="bg-foreground/20 text-white shadow-md p-5 relative rounded-xl overflow-hidden">
-        <Image
-          src={data.mainBanner}
-          alt="banner"
-          fill
-          sizes="10%"
-          className="object-cover -z-10"
-        />
-        <div className="relative z-10">
-          <h2 className="font-cinzel font-bold text-2xl">{title}</h2>
-          <p className="text-dimmed text-lg mt-2">{description}</p>
-          <Link href={`/private-files/${data.id}`} className="w-max mt-5 flex">
-            <MainButton className="bg-transparent text-white border-white rounded-md !text-sm hover:!text-foreground hover:bg-white">
-              {t("readMore")}
-            </MainButton>
-          </Link>
-        </div>
-      </article>
-    )
+    <article className="bg-foreground/20 text-white shadow-md p-5 relative rounded-xl overflow-hidden">
+      <Image
+        src={data.mainBanner}
+        alt="banner"
+        fill
+        sizes="10%"
+        className="object-cover -z-10"
+      />
+      <div className="relative z-10">
+        <h2 className="font-cinzel font-bold text-2xl">{title}</h2>
+        <p className="text-dimmed text-lg mt-2">{description}</p>
+        <Link href={`/private-files/${data.id}`} className="w-max mt-5 flex">
+          <MainButton className="bg-transparent text-white border-white rounded-md !text-sm hover:!text-foreground hover:bg-white">
+            {t("readMore")}
+          </MainButton>
+        </Link>
+      </div>
+    </article>
   );
 }
